cc28b3cd69ddd97ed7965c4fa1367fcc0c39b97d
Lin153957 <91296284+Lin153957@users.noreply.github.com>
2021-10-15 14:27:19 +0900

    请问

--- syukka.php
+++ syukka.php
@@ -3,7 +3,6 @@
 【機能】
 書籍の出荷数を指定する。確定ボタンを押すことで確認画面へ出荷個数を引き継いで遷移す
 る。
-
 【エラー一覧（エラー表示：発生条件）】
 このフィールドを入力して下さい(吹き出し)：出荷個数が未入力
 出荷する個数が在庫数を超えています：出荷したい個数が在庫数を超えている
@@ -13,25 +12,46 @@
  * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-//if (/* ①の処理を行う */) {
-	//②セッションを開始する
-//}
+// if (/* ①の処理を行う */) {
+if (session_status()==PHP_SESSION_NONE) {
+// 	//②セッションを開始する
+session_start();
+}
 
 //③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-//if (/* ③の処理を書く */){
+if (empty($_SESSION['login'])){/* ③の処理を書く */
 	//④SESSIONの「error2」に「ログインしてください」と設定する。
+	$_SESSION['error2']='ログインして下さい。';
 	//⑤ログイン画面へ遷移する。
-//}
+	header('Location: login.php');
+	exit;
+}
 
 //⑥データベースへ接続し、接続情報を変数に保存する
-
 //⑦データベースで使用する文字コードを「UTF8」にする
+$db_name='zaiko2021_yse';
+$db_host='localhost';
+$db_port='3306';
+$db_password='2021zaiko';
+$db_user='zaiko2021_yse';
+$dsn="mysql:dbname={$db_name};host={$db_host};charset=utf8;port{$db_port}";
 
+try{
+	$pdo=new PDO($dsn,$db_user,$db_password);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+
+}catch (PDOException $e){
+	echo "接続失敗:". $e->getMessage();
+	exit;
+}
 //⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-//if(/* ⑧の処理を行う */){
-	//⑨SESSIONの「success」に「出荷する商品が選択されていません」と設定する。
-	//⑩在庫一覧画面へ遷移する。
-//}
+if(empty($_POST['books'])){/* ⑧の処理を行う */
+// 	//⑨SESSIONの「success」に「出荷する商品が選択されていません」と設定する。
+$_SESSION['success']="出荷する商品が選択されていません";
+// 	//⑩在庫一覧画面へ遷移する。
+header('Location: zaiko_ichiran.php');
+}
 
 function getId($id,$con){
 	/* 
@@ -39,9 +59,15 @@ function getId($id,$con){
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
-
+	$sql="SELECT*FROM books";
+	$books = $con->prepare('SELECT * FROM books WHERE id=$id');
+	$books->execute();
 	//⑫実行した結果から1レコード取得し、returnで値を返す。
-//}
+
+
+
+// return $row;
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -74,9 +100,13 @@ function getId($id,$con){
 		 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
 		 * 設定されていた場合はif文の中に入る。
 		 */ 
-	//	if(/* ⑬の処理を書く */){
-			//⑭SESSIONの「error」の中身を表示する。
-		//}
+		// if(/* ⑬の処理を書く */){
+		// 	//⑭SESSIONの「error」の中身を表示する。
+		if($_SESSION["error"]=!null)
+			{
+				echo $_SESSION["error"];
+			}
+		// }
 		?>
 		</div>
 		<div id="center">
@@ -96,18 +126,27 @@ function getId($id,$con){
 				/*
 				 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
 				 */
-				foreach($_PSOT['books'] as $book_id){/* ⑮の処理を書く */
+				foreach($_POST['books'] as $book_id){
 					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
-					$book = gerId($book_id,$pdo);
-			?>
-				<input type="hidden" value="<?= $book['id']?>" name="books[]">
+				$book=getId($bood_id,$pdo);
+				?>
+				<!-- <input type="hidden" value="<?php echo	"a"/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]"> -->
 				<tr>
-					<td><?= $book['id'] ?></td>
-					<td><?= $book['title'] ?></td>
-					<td><?= $book['author'] ?></td>
-					<td><?= $book['salesDate'] ?></td>
-					<td><?= $book['price'] ?></td>
-					<td><?= $book['stock'] ?></td>
+				<td id='id'><?=$book['id']?></td>
+				<td id='title'><?=$book['title']?></td>
+				<td id='author'><?=$book['author']?></td>
+				<td id='date'><?=$book['salesDate']?></td>
+				<td id='price'><?=$book['price']?></td>
+				<td id='stock'><?=$book['stock']?></td>
+
+
+
+					<td><?php echo	"a"/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
+					<td><?php echo	"a"/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
+					<td><?php echo	"a"/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
+					<td><?php echo	"a"/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
+					<td><?php echo	"a"/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
+					<td><?php echo	"a"/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
 					<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
 				</tr>
 				<?php
